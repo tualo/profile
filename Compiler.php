364f1db755490ac1c5d01ae22c35cca132a1d588
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-06-12 13:40:44 +0200

    fix require

--- Compiler.php
+++ Compiler.php
@@ -5,6 +5,8 @@ use Tualo\Office\Basic\TualoApplication;
 use Tualo\Office\ExtJSCompiler\ICompiler;
 use Tualo\Office\ExtJSCompiler\CompilerHelper;
 
+
+
 class Compiler implements ICompiler {
     public static function getFiles(){
         return CompilerHelper::getFiles(__DIR__,'profile',10003);
