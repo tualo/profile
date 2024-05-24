b76deaf9b16f5879d31e4397ce048d3c78ef7c86
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-05-24 10:17:39 +0200

    fix toast message

--- src/js/lazy/controller/ProfilePanel.js
+++ src/js/lazy/controller/ProfilePanel.js
@@ -27,13 +27,21 @@ Ext.define('Tualo.profile.lazy.controller.ProfilePanel', {
         let res= await Tualo.Fetch.post('./profile/save', this.getView().down('form').getForm().getValues() );
         console.log(res);
         if (res.success){
+            Ext.toast({
+                html: res.msg,
+                title: 'OK',
+                width: 400,
+                align: 't'
+            });
+        }else{
+            Ext.toast({
+                html: res.msg,
+                title: 'Fehler',
+                width: 400,
+                align: 't'
+            });
         }
-        Ext.toast({
-            html: res.msg,
-            title: 'Fehler',
-            width: 400,
-            align: 't'
-        });
+        
         return res;
     }
 
