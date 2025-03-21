c8789560b56a3f1378b856a8e37b67794ca01335
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-03-21 13:43:19 +0100

    not empty

--- src/Routes/Avatar.php
+++ src/Routes/Avatar.php
@@ -1,21 +1,26 @@
 <?php
+
 namespace Tualo\Office\Profile\Routes;
+
 use Tualo\Office\Basic\TualoApplication;
 use Tualo\Office\Basic\Route;
 use Tualo\Office\Basic\IRoute;
 use YoHang88\LetterAvatar\LetterAvatar;
 use LasseRafn\InitialAvatarGenerator\InitialAvatar;
 
-class Avatar implements IRoute{
-    public static function register(){
-    
-        Route::add('/classic/resources/client/avatar',function(){
+class Avatar implements IRoute
+{
+    public static function register()
+    {
+
+
+        Route::add('/classic/resources/client/avatar', function () {
             $db = TualoApplication::get('session')->getDB();
             $size = 44;
 
-            try{
+            try {
 
-                if (TualoApplication::configuration('profile','avatar','type')=='gravatar'){
+                if (TualoApplication::configuration('profile', 'avatar', 'type') == 'gravatar') {
                     $email_id = $db->dbname;
                     $default = "identicon";
                     $size = 44;
@@ -25,28 +30,42 @@ class Avatar implements IRoute{
                     header("content-type: image/jpeg");
                     TualoApplication::contenttype('image/jpeg');
                     TualoApplication::body(file_get_contents($gravatar_url));
-                }else{
+                } else {
+
                     $name =  $db->dbname;
                     $avatar = new InitialAvatar();
-                    $image = $avatar->name($name)->generate();
-                    TualoApplication::contenttype('image/png');
-                    TualoApplication::body( $image->response('png', $size) );
+                    /*
+                    $image = $avatar->name('OK')->generate();
+                    TualoApplication::contenttype('image/jpeg');
+                    TualoApplication::body($image->response('jpeg', $size));
+*/
+                    $avatar->name('Lasse Rafn')
+                        ->length(2)
+                        ->fontSize(0.5)
+                        ->size(96) // 48 * 2
+                        ->background('#8BC34A')
+                        ->color('#fff')
+                        ->generate()
+                        ->stream('png', 100);
+                    exit();
                 }
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+                exit();
                 $avatar = new InitialAvatar();
                 $image = $avatar->name('not set')->generate();
                 TualoApplication::contenttype('image/jpeg');
-                TualoApplication::body( $image->response('jpg', $size) );
+                TualoApplication::body($image->response('jpg', $size));
             }
-        },['get'],true);
+        }, ['get'], true);
 
-        Route::add('/classic/resources/client/avatar/(?P<name>[\w.\/\-]+)',function($matches){
+        Route::add('/classic/resources/client/avatar/(?P<name>[\w.\/\-]+)', function ($matches) {
             $db = TualoApplication::get('session')->getDB();
             $size = 44;
 
-            try{
+            try {
 
-                if (TualoApplication::configuration('profile','avatar','type')=='gravatar'){
+                if (TualoApplication::configuration('profile', 'avatar', 'type') == 'gravatar') {
                     $email_id = $matches['name'];
                     $default = "identicon";
 
@@ -55,53 +74,49 @@ class Avatar implements IRoute{
                     header("content-type: image/jpeg");
                     TualoApplication::contenttype('image/jpeg');
                     TualoApplication::body(file_get_contents($gravatar_url));
-                }else{
+                } else {
                     $name =  $matches['name'];
                     $avatar = new InitialAvatar();
                     $image = $avatar->name($name)->generate();
                     TualoApplication::contenttype('image/png');
-                    TualoApplication::body( $image->response('png', $size) );
+                    TualoApplication::body($image->response('png', $size));
                 }
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 $avatar = new InitialAvatar();
                 $image = $avatar->name('not set')->generate();
                 TualoApplication::contenttype('image/jpeg');
-                TualoApplication::body( $image->response('jpg', $size) );
+                TualoApplication::body($image->response('jpg', $size));
             }
+        }, ['get'], true);
 
-        },['get'],true);
-
-        Route::add('/classic/resources/profile/avatar',function(){
+        Route::add('/classic/resources/profile/avatar', function () {
 
             $db = TualoApplication::get('session')->getDB();
             $size = 44;
-            try{
+            try {
 
-                if (TualoApplication::configuration('profile','avatar','type')=='gravatar'){
+                if (TualoApplication::configuration('profile', 'avatar', 'type') == 'gravatar') {
 
-                    $email_id = $db->singleValue('select getSessionUser() v',[ ],'v');
+                    $email_id = $db->singleValue('select getSessionUser() v', [], 'v');
                     $default = "identicon";
-                    
+
                     $gravatar_url = "http://www.gravatar.com/avatar/" . md5(strtolower(trim($email_id))) . "?d=" . urlencode($default) . "&s=" . $size;
                     TualoApplication::contenttype('image/jpeg');
                     TualoApplication::body(file_get_contents($gravatar_url));
-
-                }else{
-                    $name =  $db->singleValue('select getSessionUserFullName() n',[],'n');
-                    if (trim($name)=='') $name = $db->singleValue('select getSessionUser() n',[],'n');
+                } else {
+                    $name =  $db->singleValue('select getSessionUserFullName() n', [], 'n');
+                    if (trim($name) == '') $name = $db->singleValue('select getSessionUser() n', [], 'n');
                     $avatar = new InitialAvatar();
                     $image = $avatar->name($name)->generate();
                     TualoApplication::contenttype('image/png');
-                    TualoApplication::body( $image->response('png', $size) );
+                    TualoApplication::body($image->response('png', $size));
                 }
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 $avatar = new InitialAvatar();
                 $image = $avatar->name('not set')->generate();
                 TualoApplication::contenttype('image/jpeg');
-                TualoApplication::body( $image->response('jpg', $size) );
+                TualoApplication::body($image->response('jpg', $size));
             }
-
-
-        },['get'],true);
+        }, ['get'], true);
     }
 }
