1cde8db2ee26c82dd8e8d892d617a335364e683b
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-09-10 11:46:21 +0200

    Version bump to 1.1.1

--- src/Routes/JsLoader.php
+++ src/Routes/JsLoader.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tualo\Office\Profile\Routes;
 
 use Tualo\Office\Basic\TualoApplication as App;
@@ -6,16 +7,19 @@ use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 use Tualo\Office\Basic\RouteSecurityHelper;
 
-class JsLoader implements IRoute{
-    public static function register(){
-        BasicRoute::add('/jsprofile/(?P<file>[\w.\/\-]+).js',function($matches){
+class JsLoader implements IRoute
+{
+    public static function register()
+    {
+        BasicRoute::add('/jsprofile/(?P<file>[\w.\/\-]+).js', function ($matches) {
             RouteSecurityHelper::serveSecureStaticFile(
                 $matches['file'] . '.js',
                 dirname(__DIR__, 1) . '/js/lazy/',
                 ['js'],
-                ['application/javascript']
+                [
+                    'js' => 'application/javascript',
+                ]
             );
-        },['get'],false);
-
+        }, ['get'], false);
     }
 }
